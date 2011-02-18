6356db961095d3de69cb3b2548f4b7f15fe1636b
Shireesh Anjal <shireesh@gluster.com>
2011-02-18 19:23:09 +0530

    gluster management server headless build

--- gmc-checkout.sh
+++ gmc-checkout.sh
@@ -1,6 +1,6 @@
 # cleans the workspace and checks out gmc projects in to it
 
 cd ${WORKSPACE}
-rm -rf *
+rm -rf gmc-src gmc-target
 git clone /data/private/gmc-src.git
 git clone /data/private/gmc-target.git
