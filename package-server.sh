d459d15366f8831bf3d87a4a4fd8188cba1178f2
Shireesh Anjal <shireesh@gluster.com>
2011-02-23 20:23:42 +0530

    script for packaging the gluster management server

---
+++ package-server.sh
@@ -0,0 +1,50 @@
+WAR_NAME="glustermc.war"
+SERVER_DIST_DIR="${WORKSPACE}/../../gluster-management-server/lastSuccessful"
+
+prepare-dist-dir()
+{
+	if [ -d ${WAR_NAME} ]; then
+		rm -rf ${WAR_NAME}
+	fi
+	mkdir ${WAR_NAME}
+}
+
+get-server-war()
+{
+	cd ${WAR_NAME}
+	WAR_FILE=`find -L ${SERVER_DIST_DIR} -name ${WAR_NAME}`
+	jar xvf ${WAR_FILE}
+	cd -
+}
+
+get-dist()
+{
+	ARCH=${1}
+	OS=${2}
+	WS=${3}
+
+	OUT_DIR="${WORKSPACE}/../../gluster-management-console/workspace/arch/${ARCH}/os/${OS}/ws/${WS}/buckminster.output/com.gluster.storage.management.gui.feature.webstart*.feature/glustermc"
+	NEW_DIR=${WAR_NAME}/${OS}.${WS}.${ARCH}
+	cp -R ${OUT_DIR} ${NEW_DIR}
+}
+
+get-console-dists()
+{
+	get-dist x86 win32 win32
+	get-dist x86_64 win32 win32
+	get-dist x86 linux gtk
+	get-dist x86_64 linux gtk
+	get-dist x86 macosx cocoa
+	get-dist x86_64 macosx cocoa
+}
+
+#---------------------------------------------
+# Main Action Body
+#---------------------------------------------
+echo "Packaging Gluster Management Server..."
+
+prepare-dist-dir
+get-server-war
+get-console-dists
+
+echo "Done!"
