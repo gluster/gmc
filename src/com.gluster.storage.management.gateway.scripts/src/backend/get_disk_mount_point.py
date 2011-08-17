8755a8a49722a47c45e8016d2d633c3e359f990e
Tim <timothyasir@gluster.com>
2011-08-17 18:15:39 +0530

    Enhanced get_disk_mount_point.py to use readFsTab function from FsTabUtils and added license information

--- src/com.gluster.storage.management.gateway.scripts/src/backend/get_disk_mount_point.py
+++ src/com.gluster.storage.management.gateway.scripts/src/backend/get_disk_mount_point.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python
-#  Copyright (C) 2010 Gluster, Inc. <http://www.gluster.com>
-#  This file is part of Gluster Storage Platform.
+#  Copyright (C) 2011 Gluster, Inc. <http://www.gluster.com>
+#  This file is part of Gluster Management Gateway.
 #
-#  Gluster Storage Platform is free software; you can redistribute it
-#  and/or modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 3 of
-#  the License, or (at your option) any later version.
-#
-#  Gluster Storage Platform is distributed in the hope that it will be
-#  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-#  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see
-#  <http://www.gnu.org/licenses/>.
 
 import os
+import sys
+p1 = os.path.abspath(os.path.dirname(sys.argv[0]))
+p2 = "%s/common" % os.path.dirname(p1)
+if not p1 in sys.path:
+    sys.path.append(p1)
+if not p2 in sys.path:
+    sys.path.append(p2)
 import Utils
-from DiskUtils import *
+import FsTabUtils
 from XmlHandler import ResponseXml
 
 
@@ -32,7 +26,7 @@ def getmountpoint(path):
     rs = ResponseXml()
     mountPoint = None
 
-    for line in readFsTab():
+    for line in FsTabUtils.readFsTab():
         if path.startswith(line['MountPoint']):
             if not mountPoint:
                 mountPoint = line['MountPoint']
