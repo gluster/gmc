adbf8e51dd5d2eadd98823d42f8842d9bd6910af
Shireesh Anjal <shireesh@gluster.com>
2011-08-22 20:44:12 +0530

    Modified to make sure that the script can be called from any directory

--- src/com.gluster.storage.management.gateway.scripts/src/gateway/gmg-reset-password.sh
+++ src/com.gluster.storage.management.gateway.scripts/src/gateway/gmg-reset-password.sh
@@ -10,12 +10,17 @@ if [ $# -ne 1 ]; then
 	exit ${USAGE_ERR}
 fi
 
+CURR_DIR=${PWD}
+SCRIPT_PATH=`readlink -f ${0}`
+GLUSTERMG_DIR=`dirname ${SCRIPT_PATH}`
+
 # Main action body
+cd ${GLUSTERMG_DIR}
 cd ..
 for FILE in WEB-INF/lib/*.jar
 do
 	export CLASSPATH=${CLASSPATH}:${PWD}/${FILE}
 done
 export CLASSPATH=${PWD}/WEB-INF/classes:${CLASSPATH}
-cd -
+cd ${CURR_DIR}
 java com.gluster.storage.management.gateway.utils.PasswordManager reset ${1}
