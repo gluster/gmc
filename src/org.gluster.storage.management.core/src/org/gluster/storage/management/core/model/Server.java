e44722d0e0fc575208e915bad0758ba57605a846
Shireesh Anjal <shireesh@gluster.com>
2011-12-02 16:17:28 +0530

    Added copyright notice in all sources.

--- src/org.gluster.storage.management.core/src/org/gluster/storage/management/core/model/Server.java
+++ src/org.gluster.storage.management.core/src/org/gluster/storage/management/core/model/Server.java
@@ -1,18 +1,19 @@
 /*******************************************************************************
+ * Copyright (c) 2006-2011 Gluster, Inc. <http://www.gluster.com>
  * This file is part of Gluster Management Console.
  *
- * Gluster Management Console is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *  
- * Gluster Management Console is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
- * for more details.
- *  
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see
+ * Gluster Management Console is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Gluster Management Console is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
  * <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 package org.gluster.storage.management.core.model;
--- src/org.gluster.storage.management.gateway/src/org/gluster/storage/management/gateway/services/GlusterInterface.java
+++ src/org.gluster.storage.management.gateway/src/org/gluster/storage/management/gateway/services/GlusterInterface.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2011 Gluster, Inc. <http://www.gluster.com>
+ * This file is part of Gluster Management Gateway.
+ *
+ * Gluster Management Gateway is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Gluster Management Gateway is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package org.gluster.storage.management.gateway.services;
 
 import java.util.List;
